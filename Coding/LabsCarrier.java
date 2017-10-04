137ab3159a24a7972087a591d63e61b928274780
amitgarg3132 <amitgarg3132@gmail.com>
2017-10-04 14:56:47 +0530

    Update LabsCarrier.java

--- Coding/LabsCarrier.java
+++ Coding/LabsCarrier.java
@@ -2,7 +2,7 @@ package com.walmartlabs.services.api.dcc.dto;
 
 
 import javax.xml.bind.annotation.XmlAccessType;
-asdfasd
+asadsfadsfadf
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
